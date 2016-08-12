ef900bc572f839ab70ee1bd3cc39633242229ea8
kenichiro abe <moken51mo@hotmail.co.jp>
2016-08-12 17:10:00 +0000

    have、want

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -2,6 +2,8 @@ class OwnershipsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    
+    
     if params[:item_code]
       @item = Item.find_or_initialize_by(item_code: params[:item_code])
     else
@@ -11,7 +13,11 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      items = RakutenWebService::Ichiba::Item.search(
+        item_code: params[:item_code]
+      )
+
+      binding.pry
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -26,6 +32,12 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
     
+    if params[:type] == "Have"
+      current_user.have(@item)
+    elsif params[:type] == "Want"
+      current_user.want(@item)
+    end
+    
 
   end
 
@@ -35,6 +47,12 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
+    
+    if params[:type] == "Have"
+      current_user.unhave(@item)
+    elsif params[:type] == "Want"
+      current_user.unwant(@item)
+    end
 
   end
 end
